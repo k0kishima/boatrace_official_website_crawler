8434bac6edc46b0bb7086b8b26265ff211737d77
Johndoe <56298669+togane@users.noreply.github.com>
2020-08-18 07:28:47 +0900

    implement crawl racer profile service

--- app/repositories/fundamental_data_repository.rb
+++ app/repositories/fundamental_data_repository.rb
@@ -31,6 +31,32 @@ module FundamentalDataRepository
       true
     end
 
+    def update_racer(racer)
+      connection = ConnectionBuilder.build(BASE_URL)
+      response = connection.post do |req|
+        req.url 'api/internal/v1/racers/create_or_update_many'
+        req.body = {
+            access_token: APP_TOKEN,
+            racers: [
+              # 現行バージョンではgenderはこのメソッドコールするパーサーで取れないのでここでは属性に入れてない
+              {
+                  registration_number: racer.registration_number,
+                  last_name: racer.last_name,
+                  first_name: racer.first_name,
+                  term: racer.term,
+                  birth_date: racer.birth_date,
+                  branch_id: racer.branch_id,
+                  birth_prefecture_id: racer.born_prefecture_id,
+                  height: racer.height,
+                  status: racer.status,
+              }
+            ]
+        }
+      end
+      handle_response(response)
+      true
+    end
+
     def create_or_update_many_events(events)
       connection = ConnectionBuilder.build(BASE_URL)
       response = connection.post do |req|
