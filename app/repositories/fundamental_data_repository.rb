d8b8fcf6baa40b95bcdf9293dde8bcba25847fa1
Johndoe <56298669+togane@users.noreply.github.com>
2020-08-19 10:10:37 +0900

    implement crawl race service

--- app/repositories/fundamental_data_repository.rb
+++ app/repositories/fundamental_data_repository.rb
@@ -69,7 +69,6 @@ module FundamentalDataRepository
       true
     end
 
-
     def create_or_update_many_events(events)
       connection = ConnectionBuilder.build(BASE_URL)
       response = connection.post do |req|
@@ -84,6 +83,31 @@ module FundamentalDataRepository
                   grade: event.grade,
                   kind: event.kind,
                   status: event.status,
+              }
+            end
+        }
+      end
+      handle_response(response)
+      true
+    end
+
+    def create_or_update_many_races(races)
+      connection = ConnectionBuilder.build(BASE_URL)
+      response = connection.post do |req|
+        req.url 'api/internal/v1/races/create_or_update_many'
+        req.body = {
+            access_token: APP_TOKEN,
+            races: races.map do |race|
+              {
+                  stadium_tel_code: race.stadium_tel_code,
+                  date: race.date,
+                  race_number: race.race_number,
+                  title: race.title,
+                  number_of_laps: race.number_of_laps,
+                  course_fixed: race.course_fixed,
+                  use_stabilizer: race.use_stabilizer,
+                  betting_deadline_at: race.betting_deadline_at,
+                  status: race.status,
               }
             end
         }
--- app/repositories/official_website_content_repository.rb
+++ app/repositories/official_website_content_repository.rb
@@ -33,5 +33,16 @@ module OfficialWebsiteContentRepository
       }.to_query
       URI.open("#{BASE_URL}/file?#{query}")
     end
+
+    def race_information_file(version: USE_VERSION, stadium_tel_code: , date: , race_number:)
+      query = {
+          version: version,
+          page_type: :race_information_page,
+          stadium_tel_code: stadium_tel_code,
+          race_opened_on: date,
+          race_number: race_number,
+      }.to_query
+      URI.open("#{BASE_URL}/file?#{query}")
+    end
   end
 end
