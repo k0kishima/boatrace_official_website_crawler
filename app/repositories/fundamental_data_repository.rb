3a50ea87b7d8833b8b4af1975bc350ada06cacd4
Johndoe <56298669+togane@users.noreply.github.com>
2020-08-22 17:05:08 +0900

    save winners and losers when crawl race records

--- app/repositories/fundamental_data_repository.rb
+++ app/repositories/fundamental_data_repository.rb
@@ -251,6 +251,48 @@ module FundamentalDataRepository
       true
     end
 
+    def create_or_update_many_winning_race_entries(winning_race_entries)
+      connection = ConnectionBuilder.build(BASE_URL)
+      response = connection.post do |req|
+        req.url 'api/internal/v1/winning_race_entries/create_or_update_many'
+        req.body = {
+            access_token: APP_TOKEN,
+            winning_race_entries: winning_race_entries.map do |winning_race_entry|
+              {
+                  stadium_tel_code: winning_race_entry.stadium_tel_code,
+                  date: winning_race_entry.date,
+                  race_number: winning_race_entry.race_number,
+                  pit_number: winning_race_entry.pit_number,
+                  winning_trick: winning_race_entry.winning_trick,
+              }
+            end
+        }
+      end
+      handle_response(response)
+      true
+    end
+
+    def create_or_update_many_disqualified_race_entries(disqualified_race_entries)
+      connection = ConnectionBuilder.build(BASE_URL)
+      response = connection.post do |req|
+        req.url 'api/internal/v1/disqualified_race_entries/create_or_update_many'
+        req.body = {
+            access_token: APP_TOKEN,
+            disqualified_race_entries: disqualified_race_entries.map do |disqualified_race_entry|
+              {
+                  stadium_tel_code: disqualified_race_entry.stadium_tel_code,
+                  date: disqualified_race_entry.date,
+                  race_number: disqualified_race_entry.race_number,
+                  pit_number: disqualified_race_entry.pit_number,
+                  disqualification: disqualified_race_entry.disqualification,
+              }
+            end
+        }
+      end
+      handle_response(response)
+      true
+    end
+
     private
 
       def handle_response(response)
--- app/services/crawl_race_record_service.rb
+++ app/services/crawl_race_record_service.rb
@@ -3,6 +3,8 @@ class CrawlRaceRecordService
 
   def call
     FundamentalDataRepository.create_or_update_many_race_records(race_records)
+    FundamentalDataRepository.create_or_update_many_winning_race_entries(winning_race_entries)
+    FundamentalDataRepository.create_or_update_many_disqualified_race_entries(disqualified_race_entries) if disqualified_race_entries.present?
   end
 
   private
@@ -50,6 +52,40 @@ class CrawlRaceRecordService
                        time_minute: attributes.fetch(:time_minute),
                        time_second: attributes.fetch(:time_second),
                        arrival: attributes.fetch(:arrival))
+      end
+    end
+
+    WinningRaceEntry = Struct.new(:stadium_tel_code, :date, :race_number, :pit_number, :winning_trick_name, keyword_init: true) do
+      def winning_trick
+        WinningTrickFactory.create!(winning_trick_name)
+      end
+    end
+    def winning_race_entries
+      parser.parse.select do |attributes|
+        attributes.fetch(:winning_trick_name, nil).present?
+      end.map do |attributes|
+        WinningRaceEntry.new(stadium_tel_code: stadium_tel_code,
+                             date: date,
+                             race_number: race_number,
+                             pit_number: attributes.fetch(:pit_number),
+                             winning_trick_name: attributes.fetch(:winning_trick_name))
+      end
+    end
+
+    DisqualifiedRaceEntry = Struct.new(:stadium_tel_code, :date, :race_number, :pit_number, :disqualification_mark, keyword_init: true) do
+      def disqualification
+        DisqualificationFactory.create!(disqualification_mark)
+      end
+    end
+    def disqualified_race_entries
+      parser.parse.select do |attributes|
+        attributes.fetch(:disqualification_mark, nil).present?
+      end.map do |attributes|
+        DisqualifiedRaceEntry.new(stadium_tel_code: stadium_tel_code,
+                                  date: date,
+                                  race_number: race_number,
+                                  pit_number: attributes.fetch(:pit_number),
+                                  disqualification_mark: attributes.fetch(:disqualification_mark))
       end
     end
 end
