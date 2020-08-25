3d4cd91faa4241dd8399a553402bf21b53e4c8ba
Johndoe <56298669+togane@users.noreply.github.com>
2020-08-25 21:18:38 +0900

    add crawl boat settings job

---
+++ app/jobs/crawl_boat_setting_job.rb
@@ -0,0 +1,7 @@
+class CrawlBoatSettingJob < ApplicationJob
+  queue_as :high_priority
+
+  def perform(version:, stadium_tel_code:, date:, race_number:)
+    CrawlBoatSettingService.call(version: version, stadium_tel_code: stadium_tel_code, date: date, race_number: race_number)
+  end
+end
