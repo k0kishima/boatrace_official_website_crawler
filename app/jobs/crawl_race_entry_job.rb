7686a41c84cbd3383f9576ac669471e2ba0a222c
Johndoe <56298669+togane@users.noreply.github.com>
2020-08-25 19:58:00 +0900

    add crawl race entry job

---
+++ app/jobs/crawl_race_entry_job.rb
@@ -0,0 +1,7 @@
+class CrawlRaceEntryJob < ApplicationJob
+  queue_as :high_priority
+
+  def perform(version:, stadium_tel_code:, date:, race_number:)
+    CrawlRaceEntryService.call(version: version, stadium_tel_code: stadium_tel_code, date: date, race_number: race_number)
+  end
+end
