08d5d20bef72df1715ef721c4bf180d7bf9812cd
Johndoe <56298669+togane@users.noreply.github.com>
2020-08-30 09:49:14 +0900

    fix how to handle error on crawling event entries

--- app/jobs/crawl_event_entry_job.rb
+++ app/jobs/crawl_event_entry_job.rb
@@ -1,15 +1,6 @@
 class CrawlEventEntryJob < ApplicationJob
   queue_as :high_priority
 
-  # TODO:
-  # data not found と cancel を区別したい
-  # 前者は時系列的にデータ自体が存在しない（公開されていない情報を閲覧しようとしたとか）
-  # 後者はイベントやレースの中止によってデータが存在しない
-  discard_on ::ParserError::DataNotFound do |job, _|
-    page = EventEntriesPageRepository.fetch(**job.arguments.first)
-    Notification.new(type: :info).notify("below event canceled.\n#{page.origin_redirection_url}")
-  end
-
   def perform(version:, stadium_tel_code:, event_starts_on:)
     CrawlEventEntryService.call(version: version, stadium_tel_code: stadium_tel_code, event_starts_on: event_starts_on)
   end
--- app/services/crawl_event_entry_service.rb
+++ app/services/crawl_event_entry_service.rb
@@ -4,6 +4,15 @@ class CrawlEventEntryService
   def call
     # ここではupsertは使わない（パース対象のページから完全データが取れないので使えない）
     RacerRepository.create_many(racers)
+  rescue ::ParserError::DataNotFound
+    # TODO:
+    # data not found と cancel を区別したい
+    #  前者は時系列的にデータ自体が存在しない（公開されていない情報を閲覧しようとしたとか）
+    #  後者はイベントやレースの中止によってデータが存在しない
+    # という風にしたい
+    # ただ、公式サイトは後者でも「データがありません」という表記でコンテンツを返してくるので区別が難しい
+    EventRepository.make_canceled(stadium_tel_code: stadium_tel_code, starts_on: event_starts_on)
+    Notification.new(type: :info).notify("below event canceled.\n#{page.origin_redirection_url}")
   end
 
   private
