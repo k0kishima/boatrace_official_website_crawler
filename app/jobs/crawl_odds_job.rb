18c486271ae161ee0a9ddde74719aad5bd1bc3da
Johndoe <56298669+togane@users.noreply.github.com>
2020-09-02 09:20:04 +0900

    fix how to handle error when crawl odds

--- app/jobs/crawl_odds_job.rb
+++ app/jobs/crawl_odds_job.rb
@@ -1,6 +1,13 @@
 class CrawlOddsJob < ApplicationJob
   queue_as :high_priority
 
+  discard_on ::ParserError::DataNotFound do |_, e|
+    Raven.capture_exception(e)
+  end
+  discard_on ::ParserError::RaceCanceled do |_, e|
+    Raven.capture_exception(e)
+  end
+
   def perform(version:, stadium_tel_code:, date:, race_number:)
     CrawlOddsService.call(version: version, stadium_tel_code: stadium_tel_code, date: date, race_number: race_number)
   end
