7b6697be7221fb01f522a09d024e9eb3422bdefc
Johndoe <56298669+togane@users.noreply.github.com>
2020-09-05 07:06:39 +0900

    modify file reloadable module

--- app/jobs/concerns/file_reloadable.rb
+++ app/jobs/concerns/file_reloadable.rb
@@ -4,6 +4,7 @@ module FileReloadable
   extend ActiveSupport::Concern
   included do
     retry_on OpenURI::HTTPError, wait: 1.minutes, attempts: 3
+    retry_on Net::OpenTimeout, wait: 1.minutes, attempts: 3
     retry_on NoMethodError, wait: 1.minutes, attempts: 3
 
     def no_cache
