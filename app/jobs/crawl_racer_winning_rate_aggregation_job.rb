8f9880e6642ea49c329cd1afa9d19279255cc30e
Johndoe <56298669+togane@users.noreply.github.com>
2020-09-14 21:44:37 +0900

    Revert "fix how to crawl racer winning rates"
    This reverts commit 3756d5062f8a7bcd41ae02077955da2b17c020c5.

--- app/jobs/crawl_racer_winning_rate_aggregation_job.rb
+++
@@ -1,8 +0,0 @@
-class CrawlRacerWinningRateAggregationJob < ApplicationJob
-  include FileReloadable
-  queue_as :high_priority
-
-  def perform(version:, stadium_tel_code:, date:, race_number:)
-    CrawlRacerWinningRateAggregationService.call(version: version, stadium_tel_code: stadium_tel_code, date: date, race_number: race_number, no_cache: no_cache)
-  end
-end
